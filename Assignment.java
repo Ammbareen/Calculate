511f728a46391c50cdbb7c97eda182a8b5b6ff40
Ammbareen <shehlaammy1108@gmail.com>
2024-05-06 01:35:40 +0530

    Updated print() message and added addIntegers() method

--- Assignment.java
+++ Assignment.java
@@ -1,8 +1,13 @@
 public class Assignment {
     public static void main(String[] args){
+        int a,b;
         System.out.println(print());
+        System.out.println(add(a,b));
     }
     public static String print(){
-        System.out.println("Hello World");
+        System.out.println("Welcome to Git");
+    }
+    public static int add(int a,int b){
+        return a+b;
     }
 }
